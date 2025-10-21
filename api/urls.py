b573e2a858c1a42d8c54b19d1e3c0ddfe82548bd
Nick Joey Cabahug <nickjoey.cabahug@msunaawan.edu.ph>
2025-10-21 19:30:49 +0800

    Student and Employee class base view crud functions

--- api/urls.py
+++ api/urls.py
@@ -3,13 +3,18 @@ from django.urls import path
 from api.views import student_function_base_view
 from api.views import employee_function_base_view
 from api.views import student_class_base_view
+from api.views import employee_class_base_view
 
 urlpatterns = [
     path('fbv-students/', student_function_base_view.studentView),
-    path('fbv-students/<int:pk>', student_function_base_view.student),
+    path('fbv-student/<int:pk>', student_function_base_view.student),
 
     path('fbv-employees/', employee_function_base_view.employeeView),
-    path('fbv-employees/<int:pk>', employee_function_base_view.employee),
+    path('fbv-employee/<int:pk>', employee_function_base_view.employee),
 
     path('cbv-students/', student_class_base_view.Students.as_view()),
+    path('cbv-student/<int:pk>', student_class_base_view.StudentDetail.as_view()),
+
+    path('cbv-employees/', employee_class_base_view.Employees.as_view()),
+    path('cbv-employee/<int:pk>', employee_class_base_view.EmployeeDetail.as_view()),
 ]
--- api/views/employee_class_base_view.py
+++ api/views/employee_class_base_view.py
@@ -1,45 +1,45 @@
-from ..serializer import StudentSerializer
+from ..serializer import EmployeeSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from students.models import Student
+from employees.models import Employee
 from django.http import Http404
 
 # Class Base View
-class Students(APIView):
+class Employees(APIView):
     def get(self, request):
-        students = Student.objects.all()
-        serializer = StudentSerializer(students, many=True)
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     def post(self, request):
-        serializer = StudentSerializer(data=request.data)
+        serializer = EmployeeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-class StudentDetail(APIView):
+class EmployeeDetail(APIView):
     def get_object(self, pk):
         try:
-            return Student.objects.get(pk=pk)
-        except Student.DoesNotExist:
+            return Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
-        student = self.get_object(pk)
-        serializer = StudentSerializer(student)
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, pk):
-        student = self.get_object(pk)
-        serializer = StudentSerializer(student, data=request.data)
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        student = self.get_object(pk)
-        student.delete()
+        employee = self.get_object(pk)
+        employee.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
